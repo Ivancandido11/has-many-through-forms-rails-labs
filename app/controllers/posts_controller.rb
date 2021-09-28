0e7dd7f6dfe5cf6fecd0373652e4b0fdb5bd4d71
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-28 13:05:17 -0400

    Almost Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def index
@@ -16,9 +17,9 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
-  private
+private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids: [], categories_attributes: [:name])
   end
 end
