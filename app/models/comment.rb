39f151f54f8b1e94869b1fcd107ec3e71dc8c150
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-28 15:23:16 -0400

    Completed Assignment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,10 +4,11 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes["username"].blank? }
 
   def user_attributes=(user_attributes)
-    # byebug
+    return if user_attributes[:username] == ""
+
     user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
-      self.user << user
+      user = User.find_or_create_by(username: user_attribute)
+      self.update(user_id: user.id)
     end
   end
 end
